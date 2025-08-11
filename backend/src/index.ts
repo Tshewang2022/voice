fc047398950f80b69b922f456a7251b8d8fcf217
Tshewang Gyaltshen <tshewanggyaltshen0@gmail.com>
2025-08-11 16:14:54 +0600

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+
+// ready to write the production ready code
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello from TypeScript + Node.js!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
